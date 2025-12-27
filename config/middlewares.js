23867bade2a3202655b8836cf6eebb22a2905989
Yash Gupta <yash.gupta@anarish.com>
2025-12-27 13:29:35 +0530

    updated frontend url

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,19 @@
 module.exports = [
+];
+
+module.exports = [
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://cms-frontend-alpha-eight.vercel.app'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
+  },
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
